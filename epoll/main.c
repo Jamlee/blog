3523663b38400b8b92366011354e3e7f1ddfb156
jamlee <jamlee@jamlee.cn>
2020-03-30 17:59:07 +0000

    添加 c 语言、linux epoll 的内容

---
+++ epoll/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/epoll.h>
+
+
+#define SERVER_PORT    (7778)
+#define EPOLL_MAX_NUM  (2048) //最多 2048 个  fd
+#define BUFFER_MAX_LEN (4096)
+
+char buffer[BUFFER_MAX_LEN];
+
+int main() {
+    int epfd = epoll_create(1000);
+    return 0;
+}
