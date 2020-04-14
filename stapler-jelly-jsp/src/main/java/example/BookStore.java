66019ea81f8022d68adced6d8462a7fb0bd5c41b
jamlee <jamlee@jamlee.cn>
2020-04-15 02:15:30 +0800

    stapler for jenkins demo

---
+++ stapler-jelly-jsp/src/main/java/example/BookStore.java
@@ -0,0 +1,93 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2017-2020, Nanten Cloud LLC. Junhong Chen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package example;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import example.item.Book;
+import example.item.CD;
+import example.item.Item;
+import example.item.Painting;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import javax.servlet.ServletException;
+
+/**
+ * The root object serving the root URL /
+ * @author junho.chen@outlook.com
+ *
+ */
+public class BookStore {
+
+	/**
+	 * Stores various objects scoped to {@link BookStore}
+	 */
+	public static BookStore theStore = new BookStore();
+
+	public String getMyString() {
+		return "Hello, Stapler Jelly!";
+	}
+
+	public String getNoJelly() {
+		return "getNoJelly";
+	}
+
+	public String getOk() {
+		return "ok";
+	}
+
+	public String doNoJelly() {
+		return "There's no jelly associated with this doNoJelly() method";
+	}
+
+	public Map<String, Item> getItems() {
+		Map<String, Item> items = new HashMap<String, Item>();
+		items.put("book", new Book("Brave heart"));
+		items.put("cd", new CD("Mozart"));
+		items.put("painting", new Painting("Monet",985000));
+		return items;
+	}
+
+	/**
+	 * Polymorphism and parameter test.
+	 * @param name
+	 * @return
+	 */
+	public Item getItem(String name) {
+		if(name == null || name.equals("")) {
+			return new Book("The Chaos Classics");
+		}
+		if("painting".equals(name)) {
+			return new Painting("Monet - "+name,985555);
+		}
+		if(name.startsWith("b")) {
+			return new Book(name);
+		}
+		return new CD(name);
+	}
+}
---
+++ stapler-jelly-jsp/src/main/webapp/WEB-INF/side-files/example/BookStore/ok.jsp
@@ -0,0 +1,11 @@
+<%@page import="java.util.ArrayList"%>
+<html>
+<head>
+    <title>Java Code Geeks Snippets - Sample JSP Page</title>
+</head>
+<body>
+<br />
+    ${it.myString}
+    Current date is: <%=new java.util.Date()%>
+</body>
+</html>
