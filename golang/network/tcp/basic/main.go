ca812a1e50dbe5e40ee1116a891706f9bdbae64c
jamleeli <jamleeli@tencent.com>
2021-07-09 16:18:48 +0800

    go 网络

--- golang/network/tcp/basic/main.go
+++ golang/network/tcp/basic/main.go
@@ -1,1 +1,33 @@
-package _01
+package main
+
+import (
+	"fmt"
+	"net"
+	"log"
+)
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	//
+	for {
+
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:20000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 循环
+	for {
+		// 每次建立链接时。返回一个 conn
+		conn, err := listen.Accept() // 建立连接
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		}
+		//  不要阻塞后续的链接
+		go process(conn)
+	}
+}
